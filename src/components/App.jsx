c709e16f9d59560887d6fa95d7d9ce77251d0b8e
SnehaShisNasty <katirina18@gmail.com>
2024-01-13 19:08:37 +0000

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import { Main } from './App.styled';
+
+import { Searchbar } from './searchbar/Searchbar';
+
+import { searchImgs } from './api/server';
+
+class App extends Component {
+  state = {
+    search: '',
+    page: 1,
+    loading: false,
+    list: [],
+  };
+
+  async componentDidUpdate(_, prevState) {
+    const { search, page } = this.state;
+    if (search && (search !== prevState.search || page !== prevState.page)) {
+      this.serverImgs();
+    }
+    console.log(this.state);
+  }
+
+  async serverImgs() {
+    const { search, page } = this.state;
+    try {
+      this.setState({
+        loading: true,
+      });
+      const { data } = await searchImgs(search, page);
+      console.log(data.hits);
+      this.setState(({ list }) => ({
+        list: [...list, ...data.hits],
+      }));
+    } catch (error) {
+      this.setState({
+        error: error.message,
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
+  handleSearch = ({ search }) => {
+    this.setState({
+      search,
+      list: [],
+      page: 1,
+    });
+  };
+
+  render() {
+    return (
+      <Main>
+        <Searchbar onSubmit={this.handleSearch}></Searchbar>
+      </Main>
+    );
+  }
+}
+export { App };
---
+++ src/components/searchbar/Searchbar.jsx
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+import { Header, Form, Button, ButtonLabel, Input } from './Searchbar.styled';
+
+class Searchbar extends Component {
+  state = {
+    search: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit({ ...this.state });
+    this.setState({
+      search: '',
+    });
+  };
+  render() {
+    return (
+      <Header>
+        <Form onSubmit={this.handleSubmit}>
+          <Button type="submit">
+            <ButtonLabel>Search</ButtonLabel>
+          </Button>
+
+          <Input
+            type="text"
+            placeholder="Search images and photos"
+            name="search"
+            onChange={this.handleChange}
+          />
+        </Form>
+      </Header>
+    );
+  }
+}
+export { Searchbar };
